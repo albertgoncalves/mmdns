07e53db9ff27f2a2fb0a8114470878d60588396d
Albert Goncalves <goncalves.albert@gmail.com>
2021-02-18 22:58:07 -0500

    no-op

--- shell.nix
+++ shell.nix
@@ -1,7 +1,7 @@
 with import <nixpkgs> {};
 mkShell {
     buildInputs = [
-        (python38.withPackages(ps: with ps; [
+        (python38.withPackages (ps: with ps; [
             beautifulsoup4
             flake8
             matplotlib
